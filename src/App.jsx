1d6d1701b0b9a6db93de961d2ab663b1cf6b8600
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2023-05-15 07:56:19 -0600

    se crean los componentes

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter,RouterProvider } from "react-router-dom"
+import Comments from "./components/Comments"
+
+const router = [
+  {
+    path:'/comment',
+    element:<Comments/>
+  }
+]
+
+function App() {
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default App
