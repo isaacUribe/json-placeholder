7899ff9e279b42ba272def85931ab2d7aa264f2b
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2023-05-15 08:07:37 -0600

    se crea el router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
 import { createBrowserRouter,RouterProvider } from "react-router-dom"
 import Comments from "./components/Comments"
+import Albums from "./components/Albums"
+import Photos from "./components/Photos"
+import Post from "./components/Post"
+import Todos from "./components/Todos"
+import Users from "./components/Users"
 
-const router = [
+const router = createBrowserRouter(
+  [
   {
-    path:'/comment',
+    path:'/comments',
     element:<Comments/>
+  },
+  {
+    path:'/albums',
+    element:<Albums/>
+  },
+  {
+    path:'/photos',
+    element:<Photos/>
+  },
+  {
+    path:'/post',
+    element:<Post/>
+  },
+  {
+    path:'/todos',
+    element:<Todos/>
+  },
+  {
+    path:'/users',
+    element:<Users/>
   }
 ]
+) 
+  
 
 function App() {
 
